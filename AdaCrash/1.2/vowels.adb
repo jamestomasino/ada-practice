306dae360dc9a061f04a36be3c03b77053341082
James Tomasino <james@tomasino.org>
2019-02-28 00:11:07 -0500

    vowels

---
+++ AdaCrash/1.2/vowels.adb
@@ -0,0 +1,27 @@
+with Ada.Text_IO;
+with Ada.Integer_Text_IO;
+
+procedure Vowels is
+  use Ada.Text_IO;
+  use Ada.Integer_Text_IO;
+  Letter      : Character;
+  Vowel_Count : Integer := 0;
+  Y_Count     : Integer := 0;
+begin
+  while not End_Of_File loop
+    Get(Letter);
+    case Letter is
+      when 'A'|'E'|'I'|'O'|'U'|
+           'a'|'e'|'i'|'o'|'u' =>
+           Vowel_Count := Vowel_Count + 1;
+
+      when 'Y'|'y' =>
+        Y_Count := Y_Count + 1;
+
+      when others =>
+        null;
+    end case;
+  end loop;
+  Put("Total number of vowels = "); Put(Vowel_Count); New_Line;
+  Put("Total number of Ys = "); Put(Y_Count); New_Line;
+end Vowels;
