016a93fe55f6f842819576761ca1537e91689a2f
James Tomasino <james@tomasino.org>
2020-02-07 10:41:57 +0000

    understanding more, cleanup of errors

--- echo-server/echo_server_multi.adb
+++ echo-server/echo_server_multi.adb
@@ -1,17 +1,19 @@
 with Ada.Text_IO;
 with Ada.IO_Exceptions;
 with GNAT.Sockets;
+
 procedure echo_server_multi is
 -- Multiple socket connections example based on Rosetta Code echo server.
- 
+
    Tasks_To_Create : constant := 3; -- simultaneous socket connections.
- 
+
 -------------------------------------------------------------------------------
 -- Use stack to pop the next free task index. When a task finishes its
 -- asynchronous (no rendezvous) phase, it pushes the index back on the stack.
    type Integer_List is array (1..Tasks_To_Create) of integer;
    subtype Counter is integer range 0 .. Tasks_To_Create;
    subtype Index is integer range 1 .. Tasks_To_Create;
+
    protected type Info is
       procedure Push_Stack (Return_Task_Index : in Index);
       procedure Initialize_Stack;
@@ -20,20 +22,20 @@ procedure echo_server_multi is
       Task_Stack   : Integer_List; -- Stack of free-to-use tasks.
       Stack_Pointer: Counter := 0;
    end Info;
- 
+
    protected body Info is
       procedure Push_Stack (Return_Task_Index : in Index) is
       begin -- Performed by tasks that were popped, so won't overflow.
          Stack_Pointer := Stack_Pointer + 1;
          Task_Stack(Stack_Pointer) := Return_Task_Index;
       end;
- 
+
       entry Pop_Stack (Get_Task_Index : out Index) when Stack_Pointer /= 0 is
       begin -- guarded against underflow.
          Get_Task_Index := Task_Stack(Stack_Pointer);
          Stack_Pointer := Stack_Pointer -  1;
-      end;         
- 
+      end;
+
       procedure Initialize_Stack is
       begin
          for I in Task_Stack'range loop
@@ -41,39 +43,35 @@ procedure echo_server_multi is
          end loop;
       end;
    end Info;
- 
+
    Task_Info : Info;
- 
+
 -------------------------------------------------------------------------------
    task type SocketTask is
       -- Rendezvous the setup, which sets the parameters for entry Echo.
       entry Setup (Connection : GNAT.Sockets.Socket_Type;
-                   Client     : GNAT.Sockets.Sock_Addr_Type;
                    Channel    : GNAT.Sockets.Stream_Access;
                    Task_Index : Index);
       -- Echo accepts the asynchronous phase, i.e. no rendezvous. When the
       -- communication is over, push the task number back on the stack.
       entry Echo;
    end SocketTask;
- 
+
    task body SocketTask is
       my_Connection : GNAT.Sockets.Socket_Type;
-      my_Client     : GNAT.Sockets.Sock_Addr_Type;
       my_Channel    : GNAT.Sockets.Stream_Access;
       my_Index      : Index;
    begin
       loop -- Infinitely reusable
          accept Setup (Connection : GNAT.Sockets.Socket_Type; 
-                       Client  : GNAT.Sockets.Sock_Addr_Type; 
                        Channel : GNAT.Sockets.Stream_Access;
                        Task_Index   : Index) do
             -- Store parameters and mark task busy.
             my_Connection := Connection;
-            my_Client     := Client;
             my_Channel    := Channel;
             my_Index      := Task_Index;
          end;
- 
+
          accept Echo; -- Do the echo communications.
          begin
             Ada.Text_IO.Put_Line ("Task " & integer'image(my_Index));
@@ -98,7 +96,7 @@ procedure echo_server_multi is
    task type SocketServer (my_Port : GNAT.Sockets.Port_Type) is
       entry Listen;
    end SocketServer;
- 
+
    task body SocketServer is
    Receiver   : GNAT.Sockets.Socket_Type;
    Connection : GNAT.Sockets.Socket_Type;
@@ -106,7 +104,7 @@ procedure echo_server_multi is
    Channel    : GNAT.Sockets.Stream_Access;
    Worker     : array (1..Tasks_To_Create) of SocketTask;
    Use_Task   : Index;
- 
+
    begin
       accept Listen;
       GNAT.Sockets.Create_Socket (Socket => Receiver);
@@ -121,7 +119,7 @@ procedure echo_server_multi is
                      Port   => my_Port));
       GNAT.Sockets.Listen_Socket (Socket => Receiver);
       Task_Info.Initialize_Stack;
-Find: loop -- Block for connection and take next free task.
+      Find: loop -- Block for connection and take next free task.
          GNAT.Sockets.Accept_Socket
            (Server  => Receiver,
             Socket  => Connection,
@@ -130,7 +128,7 @@ Find: loop -- Block for connection and take next free task.
          Channel := GNAT.Sockets.Stream (Connection);
          Task_Info.Pop_Stack(Use_Task); -- Protected guard waits if full house.
          -- Setup the socket in this task in rendezvous.
-         Worker(Use_Task).Setup(Connection,Client, Channel,Use_Task);
+         Worker(Use_Task).Setup(Connection, Channel, Use_Task);
          -- Run the asynchronous task for the socket communications.
          Worker(Use_Task).Echo; -- Start echo loop.
       end loop Find;
